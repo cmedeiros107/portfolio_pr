feb1408e51f505c80e4af880858d4a4e7ddfd419
chris <cmedeiros107@gmail.com>
2015-05-27 20:49:56 -0400

    Please enter the commit message for your changes. Lines starting

---
+++ myFirstAngularApp/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myFirstAngularAppApp
+ * @description
+ * # myFirstAngularAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myFirstAngularAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
